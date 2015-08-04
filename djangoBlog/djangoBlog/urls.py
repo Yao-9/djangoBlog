c26ebaef958273bf3f0ab847958a0636d96b3695
Yao <zhao.jerry91@gmail.com>
2015-08-04 12:35:04 -0500

    change url to default one

--- djangoBlog/djangoBlog/urls.py
+++ djangoBlog/djangoBlog/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^blogengine/', include('blogengine.urls')),
+    url(r'^', include('blogengine.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
